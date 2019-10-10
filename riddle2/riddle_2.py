83e10f8faa390772d07a28f412fd2f9de73dff24
Etienne Grall <etienneg@theodo.fr>
2019-10-10 20:55:59 +0200

    Update riddle 2

--- riddle2/riddle_2.py
+++ riddle2/riddle_2.py
@@ -1,8 +1,20 @@
 import json
 
-FILE_NAME = './addresses.json'
+# Renseigne les infos de l'énigme précédente
+USERNAME = "myUserName"
+ID = "123"
 
-with open(FILE_NAME) as json_file:
-    data = json.load(json_file)
-    
-    print(data['myUser-123'])
+# Indique le nom du fichier contenant les coordonnées de tous les informateurs
+FILE_NAME = "./addresses.json"
+
+with open(FILE_NAME) as addresses_file:
+    addresses = json.load(addresses_file)
+
+    # Reconstruis la clé associée à ton username
+    username_key = USERNAME + "-" + ID
+
+    # Récupère les coordonnées de l'informateur
+    address_to_visit = addresses[username_key]
+
+    # Affiche les coordonnées de l'informateur
+    print(address_to_visit)
