edfefb1d0d90d8355a9a567081e6a5ac08f65ef5
jordanl <jordanl@theodo.fr>
2019-10-10 20:22:00 +0200

    Add blank riddle 1

---
+++ riddle_1.py
@@ -0,0 +1,14 @@
+import requests
+
+BASE_URI = "http://ripper.theo.do"
+
+
+ENDPOINT = ""  # A compléter
+ID = ""  # A compléter
+URL = BASE_URI + ENDPOINT + ID
+
+response = ""  # A compléter
+
+data = response.content
+
+print(data)
