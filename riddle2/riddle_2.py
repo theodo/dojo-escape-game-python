8d385b7d67a541e9c53c3481c0091d41cb1cfe29
jordanl <jordanl@theodo.fr>
2019-10-16 14:13:52 +0200

    Remove accents in riddle 2

--- riddle2/riddle_2.py
+++ riddle2/riddle_2.py
@@ -1,20 +1,20 @@
 import json
 
-# Renseigne les infos de l'énigme précédente
+# Renseigne les infos de l'enigme precedente
 USERNAME = "myUserName"
 ID = "123"
 
-# Indique le nom du fichier contenant les coordonnées de tous les informateurs
+# Indique le nom du fichier contenant les coordonnees de tous les informateurs
 FILE_NAME = "./addresses.json"
 
 with open(FILE_NAME) as addresses_file:
     addresses = json.load(addresses_file)
 
-    # Reconstruis la clé associée à ton username
+    # Reconstruis la cle associee a ton username
     username_key = USERNAME + "-" + ID
 
-    # Récupère les coordonnées de l'informateur
+    # Recupere les coordonnees de l'informateur
     address_to_visit = addresses[username_key]
 
-    # Affiche les coordonnées de l'informateur
+    # Affiche les coordonnees de l'informateur
     print(address_to_visit)
