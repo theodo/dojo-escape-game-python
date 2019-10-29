7e38d63b66b166acc5cadd6ead9b3839088c1862
EtienneGrall <etienneg@theodo.fr>
2019-10-29 20:09:53 +0100

    Create starting branch

--- riddle1/riddle_1.py
+++ riddle1/riddle_1.py
@@ -1,13 +1,1 @@
 import requests
-
-BASE_URI = "http://ripper.theo.do"
-
-ENDPOINT = "/api/get-username"
-ID = "/316"  # A remplacer avec son ID
-URL = BASE_URI + ENDPOINT + ID
-
-response = requests.get(URL)
-
-data = response.content.decode()
-
-print(data)
--- riddle5/riddle_5.py
+++ riddle5/riddle_5.py
@@ -1,26 +1,2 @@
 import requests
 import json
-
-# Get culprit name from spotify API
-HINT_URL = "https://api.spotify.com/v1/playlists"
-culprit_playlist_id = "4wRfyALTv1jNL7HScIhd1S"
-spotify_token = "BQCnevGOQcUclx-zbEg4DRZXPLmE4o2y1I135ZlRRgMHsK0y3oElz3d8ySXmS-afU8rUtmgM0rGj6BqVlAY_6jPcXkKIwN2um3ZwVINEXpDGohZWrDnFoG3eYsLvsZ7XpMTbEIiQP5lB5u4IHjZQc2eevxfToGS2zw"  # A remplacer
-
-culprit_information = requests.get(
-    HINT_URL + "/" + culprit_playlist_id,
-    headers={"Authorization": "Bearer {}".format(spotify_token)},
-).content.decode()
-
-culprit_name = json.loads(culprit_information)["owner"]["display_name"]
-
-# Send culprit name to the police
-CULPRIT_URL = "http://ripper.theo.do/api/culprit"
-user_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTcxODQ0Mjg1LCJqdGkiOiI4NjRmY2ZjZjkxZGU0N2JkOTM4MjEwYzI2YWYwNWM4OCIsInVzZXJfaWQiOjIzM30.irR-H4MsqpKg-koOaU1kctRsQQzv7cZ8TYFbt-T-09M"  # A remplacer
-
-police_answer = requests.post(
-    CULPRIT_URL,
-    data={"culprit_name": culprit_name},
-    headers={"Authorization": "Bearer {}".format(user_token)},
-).content.decode()
-
-print(police_answer)
