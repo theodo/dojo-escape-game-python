f4ca0e4e76f8378f2f5c79ad61f59866c0032293
EtienneGrall <etienneg@theodo.fr>
2019-10-29 20:44:57 +0100

    Fix riddle 4

--- riddle4/riddle_4.py
+++ riddle4/riddle_4.py
@@ -1,18 +1,19 @@
 import requests
-from flask import Flask
+from flask import Flask, Response, request
 
 app = Flask(__name__)
 
 SERVER_MOUNTER_URL = "http://ripper.theo.do/api/server-mounted"
 
+token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTcxMjM1NDYxLCJqdGkiOiJmYmI3OWY0MTNkNWQ0ZDQ1OTUzMzNjYjgzMDE4OGNiOCIsInVzZXJfaWQiOjE0NH0.5PzBmcLonuvMS9s9s7pjxte_zc3uf1pDFCL9LK3dZIc"  # A remplacer
+ip_address = "10.231.165.12"  # A remplacer
+requests.post(
+    SERVER_MOUNTER_URL,
+    data={"ip_address": ip_address},
+    headers={"Authorization": "Bearer {}".format(token)},
+)
 
-@app.route("/")
+@app.route("/", methods=['POST'])
 def server_successfully_mounted():
-    token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTcxMjM1NDYxLCJqdGkiOiJmYmI3OWY0MTNkNWQ0ZDQ1OTUzMzNjYjgzMDE4OGNiOCIsInVzZXJfaWQiOjE0NH0.5PzBmcLonuvMS9s9s7pjxte_zc3uf1pDFCL9LK3dZIc"  # A remplacer
-    ip_address = "10.231.165.12"  # A remplacer
-
-    return requests.post(
-        SERVER_MOUNTER_URL,
-        data={"ip_address": ip_address},
-        headers={"Authorization": "Bearer {}".format(token)},
-    ).content.decode()
+    print(request.data)
+    return Response(200)
